c5119e45fb5859ad89214e53d756696c908a8cae
TechFusion78 <techfusion78@gmail.com>
2024-12-20 00:39:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ provider "azurerm" {
   subscription_id = "2101177a-d10d-46b2-82c0-49b2103ceb55"
 }
 resource "azurerm_resource_group" "rg" {
-  name = "module"
+  name = "module2323"
   location = "east us"
 }
